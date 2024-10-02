78daf0b0b85952a2eaf87bebb7c7132778fe8b43
Andrey <andron152d@gmail.com>
2024-10-02 17:41:33 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "Scanner.h"
-
+#include "Printer.h"
 int main() {
-	Scanner hp;
+	/*Scanner hp;
 	cout << "text:" << hp.getText()<<endl;
 	string user;
 	cout << "input filename: ";
 	cin >> user;
 	hp.scanFile(user);
-	cout << "text:" << hp.getText() << endl;
-
+	cout << "text:" << hp.getText() << endl;*/
+	printer::printFile("HELLO!!!!!!!!!!!!!! form Andrey","test.txt",3);
 }
