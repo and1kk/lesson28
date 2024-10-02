d240daf43043bbf42cd34fe1b5256c311dda6737
Andrey <andron152d@gmail.com>
2024-10-02 17:17:36 +0300

    add difanition

--- Scanner.cpp
+++ Scanner.cpp
@@ -2,17 +2,38 @@
 
 Scanner::Scanner()
 {
+    text = "empty";
 }
 
 void Scanner::setText(string text)
 {
+    this->text = text;
 }
 
 string Scanner::getText() const
 {
-    return string();
+    return text;
 }
 
 void Scanner::scanFile(string filename)
 {
+    ifstream file(filename);
+    if (file.is_open()) {
+        string tmp;
+        text = "";
+        while (!file.eof()) {
+            getline(file,tmp);
+            text += tmp + "\n";
+
+
+        }
+        file.close();
+        cout << "text was scaned" << endl;
+    
+    }
+    else {
+        cout << "Error" << endl;
+
+    }
+
 }
